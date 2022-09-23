c9a9abc678f05e1f7d146d17d8ed90f76d2ebb05
amarasalah <amarasalah@outlook.com>
2022-09-23 13:01:07 +0100

    add and delete

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import {Task} from 'src/app/models/task';
 
 @Component({
   selector: 'app-tasks',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
+      myTask : Task = {
+          label: '',
+          completed: false,
+        
+      };
+  
+  
+  tasks: Task[] = [];
+  task: any;
 
-  constructor() { }
+
+
+  constructor(private taskservice: TaskService) { }
 
   ngOnInit(): void {
+    this.getTasks();
+  }
+  getTasks(): void {
+    this.taskservice.findAll()
+    .subscribe(tasks=> this.tasks = tasks )
+  }
+  deleteTask(id)   {
+
+    this.taskservice.delete(id)
+    .subscribe(()=> {
+      this.tasks = this.tasks.filter(task =>task.id != id)
+  })
+
+
   }
 
+  persistTask(){
+    this.taskservice.persist(this.myTask).subscribe((task)=>{
+      this.tasks = [task,...this.tasks]})
+    }
+
+  
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,10 +1,24 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor(private httpclient: HttpClientModule) { }
+
+  apiUrl: string = 'http://localhost:5000/tasks';
+  constructor(private http: HttpClient) { }
+
+  findAll(){
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  delete(id){
+    return this.http.delete(`${this.apiUrl}/${id}`)
+  }
+  persist(task){
+    return this.http.post<Task>(`${this.apiUrl}`, task =task)
+  }
 }
